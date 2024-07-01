f2baab2fa1b3e00ee7fefed0cd57f2ad6ef531ea
Rishi <rishi@Simplomatic-India-MacBook-Air.local>
2024-07-01 15:05:38 +0530

    test

--- models/userModels.js
+++ models/userModels.js
@@ -2,7 +2,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
-////
+////////
+/////
 
 const userSchema = new mongoose.Schema({
   name: {
