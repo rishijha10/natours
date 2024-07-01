a8610967e4f6a9aac14328138476cceb90092eab
Rishi <1032200587@tcetmumbai.in>
2024-07-01 15:10:13 +0530

    test

--- models/userModels.js
+++ models/userModels.js
@@ -2,8 +2,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
-////////
-/////
 
 const userSchema = new mongoose.Schema({
   name: {
